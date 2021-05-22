e017883d40b67bd73a57b9e3e446d30da609011a
Swarn Chandrahasa Suvarna <diya-swarns@corp.systechmlr.net>
2021-05-22 22:49:25 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Header from "./components/Header";
+import Contact from "./components/Contact";
 
-function App() {
-  return (
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+class App extends Component {
+
+  render(){
+   
+    return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header branding="Contact Manager"/>
+      <Contact name="John Doe" email="s.swadrn619@gmail.com" phone="914876685"/>
+      <Contact name="Kim" email="Kim@gmail.com" phone="9148d76685"/>
     </div>
-  );
+    );
+
+  }
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+       <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+        <div className="container">
+            <a href="/" className="navbar-brand">{props.branding}</a>
+        <div>
+            <ul className="navbar-nav mar-auto">
+                <li className="nav-item">
+                    <a href="/" className="nav-link">Home</a>
+                </li>
+            </ul>
+        </div>
+        </div>
+       </nav> 
+    )
+}
+
+
+export default Header;
